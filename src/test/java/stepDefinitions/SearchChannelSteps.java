c665b96b9b5d4ac58c8400d667d07f42b337f9b5
Gunjit2024 <gunjit29@gmail.com>
2025-04-23 23:28:35 -0700

    Remove mistakenly pushed folder

--- src/test/java/stepDefinitions/SearchChannelSteps.java
+++ src/test/java/stepDefinitions/SearchChannelSteps.java
@@ -1,6 +1,7 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.*;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import pageObject.SearchChannelModule;
@@ -8,23 +9,29 @@ import pageObject.SearchChannelModule;
 import static org.testng.Assert.*;
 
 public class SearchChannelSteps extends BaseSteps {
+    SearchChannelModule searchChannel = new SearchChannelModule();
 
+    @Step("User launches the app")
     @Given("the app is launched")
     public void the_app_is_launched() {
         waitForAppLaunchAndDismissPopup();
-        SearchChannelModule searchChannel = new SearchChannelModule();
         driverHelper.clickIfVisible(searchChannel.skyTubeHamburger, 5);
     }
 
+    @Step("User searches for {string}")
     @When("the user enters {string} in the search bar")
     public void the_user_enters_in_the_search_bar(String query) {
-        driver.findElement(By.id("free.rm.skytube:id/menu_search")).click();
-        driver.findElement(By.id("free.rm.skytube:id/search_src_text")).sendKeys(query);
+        driverHelper.clickIfVisible(searchChannel.searchText, 5);
+//        driver.findElement(By.id("free.rm.skytube:id/menu_search")).click();
+        driver.findElement(searchChannel.searchText).sendKeys(query);
+
     }
 
+    @Step("User should see channel {string} in results")
     @Then("the channel {string} should appear in the results")
     public void the_channel_should_appear_in_the_results(String channelName) {
         boolean isFound = !driver.findElements(By.xpath("//*[contains(@text,'" + channelName + "')]")).isEmpty();
         assertTrue(isFound, "Channel " + channelName + " not found.");
     }
 }
+
